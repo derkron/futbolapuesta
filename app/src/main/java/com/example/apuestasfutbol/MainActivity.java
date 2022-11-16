ce1d796b49990c2a1ceb4ee1e6a694ad1421f531
amhernandezsa@cesde.net <amhernandezsa@cesde.net>
2022-11-16 17:46:35 -0500

    actualizacion

--- app/src/main/java/com/example/apuestasfutbol/MainActivity.java
+++ app/src/main/java/com/example/apuestasfutbol/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.apuestasfutbol;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,12 +8,22 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     EditText jetcodigo,jetnombre,jetciudad;
     RadioButton jrbprofesional,jrbascenso,jrbaficionado;
     CheckBox jcbactivo;
     String codigo,nombre,ciudad;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 
     @Override
@@ -34,6 +45,35 @@ public class MainActivity extends AppCompatActivity {
         codigo=jetcodigo.getText().toString();
         nombre=jetnombre.getText().toString();
         ciudad=jetciudad.getText().toString();
+
+        if(codigo.isEmpty() || nombre.isEmpty() || ciudad.isEmpty()){
+            Toast.makeText(this, "Todos los datos son requeridos", Toast.LENGTH_SHORT).show();
+            jetcodigo.requestFocus();
+        }
+        else{
+            // Create a new user with a first and last name
+            Map<String, Object> user = new HashMap<>();
+            user.put("first", "Ada");
+            user.put("last", "Lovelace");
+            user.put("born", 1815);
+
+// Add a new document with a generated ID
+            db.collection("users")
+                    .add(user)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                          //  Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                          //  Log.w(TAG, "Error adding document", e);
+                        }
+                    });
+
+        }
 
 
     }
